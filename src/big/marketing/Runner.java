a7528668a9ae4afa9a05a10164fea33a96320749
Juhani Jaakkola <juhani@jaakkola.net>
2013-12-10 21:45:11 +0100

    Created node class that stores network nodes and information from network description

--- src/big/marketing/Runner.java
+++ src/big/marketing/Runner.java
@@ -1,8 +1,39 @@
 package big.marketing;
 
+import big.marketing.data.node;
+
 public class Runner {
 
 	public static void main(String[] args) {
+
+		
+		// THIS IS JUST A TEST TO SEE IF WORKS, DELETE THNX
+		System.out.println("\nServer");
+		node test = new node("172.10.0.2 DC01.BIGMKT1.COM	Domain controller");
+		
+		System.out.println(test.getAddress());
+		System.out.println(test.getHostName());
+		System.out.println(test.getComment());
+		System.out.println(test.isServer());
+		
+		System.out.println("\nWorkstation");		
+		
+		node test2 = new node("172.20.1.4 WSS2-04.BIGMKT2.COM");
+		
+		System.out.println(test2.getAddress());
+		System.out.println(test2.getHostName());
+		System.out.println(test2.isWorkstation());
+		System.out.println(test2.getComment());
+		
+		System.out.println("\nAdministrator");
+		
+		node test3 = new node("172.10.0.40 Administrator.BIGMKT1.COM");
+		
+		System.out.println(test3.getAddress());
+		System.out.println(test3.getHostName());
+		System.out.println(test3.isAdministator());
+		System.out.println(test3.getComment());
+		// TEST END
 
 	}
 
--- src/big/marketing/data/node.java
+++ src/big/marketing/data/node.java
@@ -1,14 +1,63 @@
 package big.marketing.data;
 
+/*
+ * #Column 1:  IP
+ *  #Column 2:  Host Name. Hosts with a "WSS" prefix are user workstations. "Administrator" is the administrator workstation. Others are servers.
+ *  #Column 3 (Optional): Comments
+ */
 public class node {
+
+	public static final int TYPE_WORKSTATION = 0;
+	public static final int TYPE_ADMINISTRATOR = 1;
+	public static final int TYPE_SERVER = 2;
+
+	private static final String regex = "\\s";
+	private static final int reglimit = 3;
+	private static final String administrator = "Administrator";
+
 	private final String address;
 	private final String hostName;
 	private final String comment;
+	private final int type;
 
-	public node(String address, String hostName, String comment) {
-		this.address = address;
-		this.hostName = hostName;
-		this.comment = comment;
+	/**
+	 * Constructor takes the one line from the BigMktNetwork.txt and uses regex
+	 * to split string to address, hostname, comment
+	 * 
+	 * @param description
+	 *           line from the txt file that describes one network node
+	 */
+	public node(String strLine) {
+
+		String[] split = strLine.split(regex, reglimit);
+
+		this.address = split[0];
+		this.hostName = split[1];
+
+		if (split.length == 3) {
+			this.comment = split[2];
+			this.type = node.TYPE_SERVER;
+			
+		} else if (split[1].contains(node.administrator)) {
+			this.type = node.TYPE_ADMINISTRATOR;
+			this.comment = null;
+			
+		} else {
+			this.type = node.TYPE_WORKSTATION;
+			this.comment = null;
+		}
+	}
+
+	public boolean isServer() {
+		return this.type == node.TYPE_SERVER;
+	}
+
+	public boolean isWorkstation() {
+		return this.type == node.TYPE_WORKSTATION;
+	}
+
+	public boolean isAdministator() {
+		return this.type == node.TYPE_ADMINISTRATOR;
 	}
 
 	public String getAddress() {
@@ -21,5 +70,9 @@ public class node {
 
 	public String getComment() {
 		return comment;
+	}
+
+	public int getType() {
+		return type;
 	}
 }
