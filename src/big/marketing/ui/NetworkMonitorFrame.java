f67fcd777ca58a46b2656185a3d420f3f8ab16e5
Juhani Jaakkola <juhani@jaakkola.net>
2013-12-12 22:52:21 +0100

    added basis for building an interface

--- src/big/marketing/ui/NetworkMonitorFrame.java
+++ src/big/marketing/ui/NetworkMonitorFrame.java
@@ -1,21 +1,33 @@
 package big.marketing.ui;
 
 import java.awt.Dimension;
+import java.awt.GridLayout;
 
 import javax.swing.JFrame;
 
-@SuppressWarnings("serial")
 public class NetworkMonitorFrame extends JFrame {
+   private static final long serialVersionUID = -8346810238547214403L;
 	private static final int FRAME_WIDTH = 1200;
 	private static final int FRAME_HEIGHT = 600;
 	private static final String FRAME_TITLE = "Big Marketing Network Monitor";
+	private static final int rows = 2;
+	private static final int cols = 2;
 	
 	
 	public NetworkMonitorFrame() {
+		addPanels();
+		
 		this.setTitle(FRAME_TITLE);
 		this.setPreferredSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLayout(new GridLayout(rows, cols));
 		this.pack();
 		this.setVisible(true);
    }
+	
+	private void addPanels() {
+		this.add(new ControlsJPanel());
+		this.add(new GraphJPanel());
+		this.add(new ParallerCoordinatesJPanel());
+	}
 }
