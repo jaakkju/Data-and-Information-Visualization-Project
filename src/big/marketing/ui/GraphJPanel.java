25c290c5bf7f9f0b1b652df2b11ea7dffcf39062
Juhani Jaakkola <juhani@jaakkola.net>
2013-12-13 16:17:03 +0100

    base of creating graph and parallercoordinates

---
+++ src/big/marketing/ui/GraphJPanel.java
@@ -0,0 +1,8 @@
+package big.marketing.ui;
+
+import javax.swing.JPanel;
+
+public class GraphJPanel extends JPanel{
+   private static final long serialVersionUID = -7417639995072699909L;
+
+}
---
+++ src/big/marketing/ui/ParallerCoordinatesJPanel.java
@@ -0,0 +1,16 @@
+package big.marketing.ui;
+
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+public class ParallerCoordinatesJPanel extends JPanel {
+	private static final long serialVersionUID = 7723958207563842249L;
+	private static final int WITDH = 200;
+	private static final int HEIGHT = 200;
+	
+
+	public ParallerCoordinatesJPanel() {
+		this.setPreferredSize(new Dimension(WITDH, HEIGHT));
+   }
+}
