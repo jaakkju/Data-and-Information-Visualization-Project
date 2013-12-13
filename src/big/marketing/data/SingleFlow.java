5225dd5129ad9ce91dc36090be5eedc172354530
oschwede <oschwede@cairon.cs.uni-paderborn.de>
2013-12-13 14:04:46 +0100

    csv reader integrated, tested singleflow

--- src/big/marketing/data/SingleFlow.java
+++ src/big/marketing/data/SingleFlow.java
@@ -1,10 +1,11 @@
 package big.marketing.data;
 
 public class SingleFlow {
+
 	private final String dateTime;
 	private final int protocol;
-	private final String sourceIP;
-	private final String destinationIP;
+	private final byte[] sourceIP;
+	private final byte[] destinationIP;
 	private final int sourcePort;
 	private final int destinationPort;
 	private final boolean last;
@@ -17,16 +18,17 @@ public class SingleFlow {
 	private final int sourcePacketCount;
 	private final int destinationPacketCount;
 	private final boolean recordForceOut;
-	public SingleFlow(String [] args) {
+
+	public SingleFlow(String[] args) {
 		super();
 		this.dateTime = args[1];
-		this.protocol = Integer.parseInt(args[4]);
-		this.sourceIP = args[5]; // split in bytes
-		this.destinationIP = args[6]; // split in bytes
+		this.protocol = Integer.parseInt(args[3]);
+		this.sourceIP = parseIP(args[5]); // split in bytes
+		this.destinationIP = parseIP(args[6]); // split in bytes
 		this.sourcePort = Integer.parseInt(args[7]);
 		this.destinationPort = Integer.parseInt(args[8]);
-		this.last = "1".equals(args[9]); //0 or 1
-		this.first = "1".equals(args[10]); //0 or 1
+		this.last = "1".equals(args[9]); // 0 or 1
+		this.first = "1".equals(args[10]); // 0 or 1
 		this.duration = Integer.parseInt(args[11]);
 		this.sourcePayloadBytes = Integer.parseInt(args[12]);
 		this.destinationPayloadBytes = Integer.parseInt(args[13]);
@@ -34,59 +36,82 @@ public class SingleFlow {
 		this.destinationTotalBytes = Integer.parseInt(args[15]);
 		this.sourcePacketCount = Integer.parseInt(args[16]);
 		this.destinationPacketCount = Integer.parseInt(args[17]);
-		this.recordForceOut = "1".equals(args[18]); //0 or 1
+		this.recordForceOut = "1".equals(args[18]); // 0 or 1
 	}
+
 	public String getDateTime() {
 		return dateTime;
 	}
+
 	public int getProtocol() {
 		return protocol;
 	}
-	public String getSourceIP() {
+
+	public byte[] getSourceIP() {
 		return sourceIP;
 	}
-	public String getDestinationIP() {
+
+	public byte[] getDestinationIP() {
 		return destinationIP;
 	}
+
 	public int getSourcePort() {
 		return sourcePort;
 	}
+
 	public int getDestinationPort() {
 		return destinationPort;
 	}
+
 	public boolean isLast() {
 		return last;
 	}
+
 	public boolean isFirst() {
 		return first;
 	}
+
 	public int getDuration() {
 		return duration;
 	}
+
 	public int getSourcePayloadBytes() {
 		return sourcePayloadBytes;
 	}
+
 	public int getDestinationPayloadBytes() {
 		return destinationPayloadBytes;
 	}
+
 	public int getSourceTotalBytes() {
 		return sourceTotalBytes;
 	}
+
 	public int getDestinationTotalBytes() {
 		return destinationTotalBytes;
 	}
+
 	public int getSourcePacketCount() {
 		return sourcePacketCount;
 	}
+
 	public int getDestinationPacketCount() {
 		return destinationPacketCount;
 	}
+
 	public boolean getRecordForceOut() {
 		return recordForceOut;
 	}
-	
-	
+
+	private byte[] parseIP(String strIP) {
+		byte[] ip = new byte[4];
+		String[] split = strIP.split("\\.");
+		for (int i = 0; i < ip.length; i++)
+			ip[i] = (byte) Integer.parseInt(split[i]);
+		return ip;
+	}
+
 	public static void main(String[] args) {
-		
+
 	}
 }
--- src/big/marketing/readers/ZipReader.java
+++ src/big/marketing/readers/ZipReader.java
@@ -7,9 +7,13 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Enumeration;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
+
+import big.marketing.data.SingleFlow;
 
 import au.com.bytecode.opencsv.CSVParser;
 import au.com.bytecode.opencsv.CSVReader;
@@ -21,38 +25,45 @@ public class ZipReader {
 	public static final String FILE_NETWORKFLOW = "VAST2013MC3_NetworkFlow.zip";
 	public static final String FILE_WEEK2DATA = "week2data_fixed.zip";
 
-	public static final int BIG_BROTHER=0, FLOW=1, IPS=2, ROWS=Integer.MAX_VALUE;
+	public static final int BIG_BROTHER = 0, FLOW = 1, IPS = 2,
+	// for production a value of 25 000 000 is sufficient
+	// for testing change this value to read only ROWS many rows
+			ROWS = 25000000;
+
 	/**
 	 * Read a csv table from the given input stream.
-	 * @param is stream to read from
-	 * @param row amount of rows to scan
+	 * 
+	 * @param is
+	 *            stream to read from
+	 * @param row
+	 *            amount of rows to scan
 	 * @return
 	 */
-	String [][] readCsvTable(InputStream is, int rows){
+	String[][] readCsvTable(InputStream is, int rows) {
 		long start = System.currentTimeMillis();
 
 		Scanner sc = new Scanner(is);
 		sc.useDelimiter("\\r");
 		String headings = sc.next().trim();
-		int columns = (headings.split("\"").length+1)/2;
-		String [][] out=null;
-		if (rows == Integer.MAX_VALUE){
+		int columns = (headings.split("\"").length + 1) / 2;
+		String[][] out = null;
+		if (rows == Integer.MAX_VALUE) {
 			ArrayList<String[]> tmpList = new ArrayList<>(2000000);
-			int i=0;
-			while (sc.hasNext()){
-//				tmpList.add(splitLine(sc.next().trim(), columns));
+			int i = 0;
+			while (sc.hasNext()) {
+				// tmpList.add(splitLine(sc.next().trim(), columns));
 				sc.next();
 				if (i++ % 100000 == 0)
 					System.out.println(i);
 			}
 			System.out.println(i);
-			System.out.println("found "+tmpList.size());
-			out = (String[][]) tmpList.toArray(new String[tmpList.size()][]); 
-		}else{
-			
+			System.out.println("found " + tmpList.size());
+			out = (String[][]) tmpList.toArray(new String[tmpList.size()][]);
+		} else {
+
 		}
 		sc.close();
-		System.out.println((System.currentTimeMillis()-start));
+		System.out.println((System.currentTimeMillis() - start));
 		return out;
 	}
 
@@ -61,8 +72,11 @@ public class ZipReader {
 	 * that are read in are specified by the regex streamName. This may be a
 	 * single filename or a regex matching several files.
 	 * 
-	 * @param zipFile name of the actual zip file
-	 * @param streamName path of the file inside the zip file (may be a regex for several files)
+	 * @param zipFile
+	 *            name of the actual zip file
+	 * @param streamName
+	 *            path of the file inside the zip file (may be a regex for
+	 *            several files)
 	 * @return read rows of the
 	 */
 	String[][] readFromZip(String zipFile, String streamName) {
@@ -86,8 +100,8 @@ public class ZipReader {
 					System.out.println("Found " + entry.getName());
 					long start = System.currentTimeMillis();
 					readCSVStream(is);
-//					result = readCsvTable(is, ROWS);
-					System.out.println(System.currentTimeMillis()-start);
+					// result = readCsvTable(is, ROWS);
+					System.out.println(System.currentTimeMillis() - start);
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
@@ -107,87 +121,91 @@ public class ZipReader {
 	 * additional quotes, that have to be removed. All other data can be simply
 	 * split at each ",".
 	 * 
-	 * @param entry line of the table to split
-	 * @param columns amount of columns in this entry
+	 * @param entry
+	 *            line of the table to split
+	 * @param columns
+	 *            amount of columns in this entry
 	 * @return array that contains one string for each cell
 	 */
 	String[] splitLine(String entry, int columns) {
 		if (columns != 14)
 			return entry.split(",");
-		
-		String [] cleanedSplit = new String[columns];
+
+		String[] cleanedSplit = new String[columns];
 		String[] rawSplit = entry.split("\"");
-		for (int i=0;i<cleanedSplit.length;i++){
-			cleanedSplit[i] = rawSplit[ i*2 +1];
-		}		
-		
+		for (int i = 0; i < cleanedSplit.length; i++) {
+			cleanedSplit[i] = rawSplit[i * 2 + 1];
+		}
+
 		return cleanedSplit;
 	}
 
 	public static void main(String[] args) {
-		 ZipReader r = new ZipReader();
+		ZipReader r = new ZipReader();
 		// some testing
-		String [][] test = null;
-//		test = r.read(BIG_BROTHER, 1); 	//  3407968 lines
-//		test = r.read(BIG_BROTHER, 2); 	//  2165508 lines
-		test = r.read(FLOW, 0); //	chunk1 15172768 lines
-//								//	chunk2 21526139 lines
-//								//	chunk3  9439406 lines
-		test = r.read(FLOW, 2 ); 		// 23258686 lines
-//		test = r.read(IPS, 2); 			// 16600932 lines
-		
+		String[][] test = null;
+		// test = r.read(BIG_BROTHER, 1); // 3407968 lines
+		// test = r.read(BIG_BROTHER, 2); // 2165508 lines
+		test = r.read(FLOW, 0); // chunk1 15172768 lines
+		// // chunk2 21526139 lines
+		// // chunk3 9439406 lines
+		test = r.read(FLOW, 2); // 23258686 lines
+		// test = r.read(IPS, 2); // 16600932 lines
+
 	}
-	
+
 	// TODO Here is a nicer way to read CSV files with a opencsv reader
-	
+
 	/**
 	 * reading inputstream with opencsv reader that returns a array of values
 	 * http://opencsv.sourceforge.net/
-	 * @param in input stream to read
+	 * 
+	 * @param in
+	 *            input stream to read
 	 * @throws IOException
 	 */
 	public void readCSVStream(InputStream in) throws IOException {
 		boolean STOP = false;
 		CSVReader reader = new CSVReader(new InputStreamReader(in));
 		String[] nextLine;
-		int i=0;
-//		CSVParser csvp = new CSVParser(',');
-//		
-//		Scanner sc = new Scanner(in);
-//		sc.useDelimiter("\\r");
-//		String [][] out = new String[ROWS][];
-//		for (i=0;i<ROWS;i++){
-//			nextLine =csvp.parseLine(sc.next());
-//			if (!"0".equals(nextLine[9]) || !"0".equals(nextLine[10]) || !"0".equals(nextLine[18]) )
-//				System.out.println(nextLine[9] + "\t"+nextLine[10]+"\t"+nextLine[18]);
-//		}
-		
-		
-//		String [][] out = new String[ROWS][];
-		
-		// for more than 100 000 lines, reader.readNext gives a heap error
-		// we have 63 000 000 lines in total, so this doesn't work
+		int i = 0;
+
+		// discard first line with descriptions
+		reader.readNext();
+
+		// String [][] out = new String[ROWS][];
 		while ((nextLine = reader.readNext()) != null && !STOP) {
 			// nextLine[] is an array of values from the line
-			// Before passing an array to HealthMessage constructor we hae to take some indexes away
+			// Before passing an array to HealthMessage constructor we hae to
+			// take some indexes away
 			// Removing element with ArrayUtils.removeElement(array, element)
-//			System.out.println(Arrays.toString(nextLine));
-//			System.out.println(nextLine.length);
-			if (!"0".equals(nextLine[18]) && !"1".equals(nextLine[18]))
-				System.out.println("18"+nextLine[18]);
-			if (!"0".equals(nextLine[10]) && !"1".equals(nextLine[10]))
-				System.out.println("10"+nextLine[10]);
-			if (!"0".equals(nextLine[9]) && !"1".equals(nextLine[9]))
-				System.out.println("9"+nextLine[9]);
-//			
-			//			out[i]=nextLine;
-			i++;
-			if (++i % 1000000 == 0)
+			// System.out.println(Arrays.toString(nextLine));
+			// System.out.println(nextLine.length);
+			handleRow(nextLine);
+			if (++i % 100000 == 0)
 				System.out.println(i);
-//			if (i>=ROWS)
-//				STOP=true;
+			// if (i>=ROWS)
+			// STOP=true;
 		}
 		reader.close();
+	}
+
+	// ArrayList<SingleFlow> list = new ArrayList<>(20000000);
+	private void handleRow(String[] nextLine) {
+
+		// boolean mode=false;
+		// modify and fill data structures here
+		// list.add(new SingleFlow(nextLine));
+		// if (!mode)
+		// mode = nextLine[9].equals("1");
+		// if (mode &&
+		// nextLine[5].equals("172.10.1.109")
+		//
+		// && nextLine[6].equals("10.199.250.2"))
+		// System.out.println(Arrays.toString(nextLine));
+		// if (mode)
+		// mode = nextLine[9].equals("1");
+
 	}
 
 	public String[][] read(int type, int week) {
