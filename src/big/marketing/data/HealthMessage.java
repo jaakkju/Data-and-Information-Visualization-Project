beb1492b5c8e13a7594c77a5a39e9a7e891a2e5b
Juhani Jaakkola <juhani@jaakkola.net>
2013-12-19 14:07:51 +0100

    added this. when refering properties of the object

--- src/big/marketing/data/HealthMessage.java
+++ src/big/marketing/data/HealthMessage.java
@@ -5,15 +5,16 @@ import com.mongodb.DBObject;
 
 /**
  * Class stores the information from a single network health message from a node
+ * 
  * @author jaakkju
- *
+ * 
  */
-public class HealthMessage implements DBWritable{
-	
-	public static final int STATUS_GOOD = 1; 
+public class HealthMessage implements DBWritable {
+
+	public static final int STATUS_GOOD = 1;
 	public static final int STATUS_WARNING = 2;
 	public static final int STATUS_PROBLEM = 3;
-	
+
 	public static final int CONN_OK = 1;
 	public static final int CONN_NOK = 0;
 
@@ -29,25 +30,28 @@ public class HealthMessage implements DBWritable{
 	private final int loadAverage;
 	private final int physicalMemoryUsage;
 	private final int connMade;
-	
+
 	/**
-	 * @param args healthMessage values string array [id, hostname, serviceName, statusVal, receivedFrom, currentTime, diskUsage, pageFileUsage, loadAverage, numProcs, physicalMemomyUsage, connMade]
+	 * @param args
+	 *           healthMessage values string array [id, hostname, serviceName,
+	 *           statusVal, receivedFrom, currentTime, diskUsage, pageFileUsage,
+	 *           loadAverage, numProcs, physicalMemomyUsage, connMade]
 	 */
 	public HealthMessage(String[] args) {
-	   this.id 						= Integer.valueOf(args[0]);
-	   this.hostname 				= args[1];
-	   this.serviceName 			= args[2];
-	   this.statusVal 			= Integer.valueOf(args[3]);
-	   this.receivedFrom 		= args[4];
-	   this.currentTime 			= Integer.valueOf(args[5]);
-	   this.diskUsage 			= Integer.valueOf(args[6]);
-	   this.pageFileUsage 		= Integer.valueOf(args[7]);
-	   this.numProcs 				= Integer.valueOf(args[8]);
-	   this.loadAverage			= Integer.valueOf(args[9]);
-	   this.physicalMemoryUsage = Integer.valueOf(args[10]);
-	   this.connMade 				= Integer.valueOf(args[11]);
-   }
-	
+		this.id = Integer.valueOf(args[0]);
+		this.hostname = args[1];
+		this.serviceName = args[2];
+		this.statusVal = Integer.valueOf(args[3]);
+		this.receivedFrom = args[4];
+		this.currentTime = Integer.valueOf(args[5]);
+		this.diskUsage = Integer.valueOf(args[6]);
+		this.pageFileUsage = Integer.valueOf(args[7]);
+		this.numProcs = Integer.valueOf(args[8]);
+		this.loadAverage = Integer.valueOf(args[9]);
+		this.physicalMemoryUsage = Integer.valueOf(args[10]);
+		this.connMade = Integer.valueOf(args[11]);
+	}
+
 	public int getId() {
 		return id;
 	}
@@ -95,22 +99,22 @@ public class HealthMessage implements DBWritable{
 	public int getConnMade() {
 		return connMade;
 	}
-	public DBObject asDBObject(){
+
+	public DBObject asDBObject() {
 		BasicDBObject bdbo = new BasicDBObject();
-		bdbo.append("id", id);
-		bdbo.append("hostname", hostname);
-		bdbo.append("serviceName", serviceName);
-		bdbo.append("statusVal", statusVal);
-		bdbo.append("receivedFrom", receivedFrom);
-		bdbo.append("currentTime", currentTime);
-		bdbo.append("diskUsage", diskUsage);
-		bdbo.append("pageFileUsage", pageFileUsage);
-		bdbo.append("numProcs", numProcs);
-		bdbo.append("loadAverage", loadAverage);
-		bdbo.append("physicalMemoryUsage", physicalMemoryUsage);
-		bdbo.append("connMade", connMade);
+		bdbo.append("id", 				this.id);
+		bdbo.append("hostname", 		this.hostname);
+		bdbo.append("serviceName", 	this.serviceName);
+		bdbo.append("statusVal", 		this.statusVal);
+		bdbo.append("receivedFrom", 	this.receivedFrom);
+		bdbo.append("currentTime", 	this.currentTime);
+		bdbo.append("diskUsage", 		this.diskUsage);
+		bdbo.append("pageFileUsage", 	this.pageFileUsage);
+		bdbo.append("numProcs", 		this.numProcs);
+		bdbo.append("loadAverage", 	this.loadAverage);
+		bdbo.append("physicalMemoryUsage", this.physicalMemoryUsage);
+		bdbo.append("connMade", 		this.connMade);
 		return bdbo;
-		
-		
+
 	}
 }
