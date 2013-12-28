7d3f3888fba692dcd943764c3a0c3720f6c90cea
Juhani Jaakkola <juhani@jaakkola.net>
2013-12-28 13:32:27 +0200

    renamed frame and added new application name to title

--- src/big/marketing/view/PCoordinatesJPanel.java
+++ src/big/marketing/view/PCoordinatesJPanel.java
@@ -8,14 +8,14 @@ import javax.swing.JPanel;
 
 import big.marketing.controller.DataController;
 
-public class ParallelCoordinatesJPanel extends JPanel implements Observer {
+public class PCoordinatesJPanel extends JPanel implements Observer {
 	private static final long serialVersionUID = 7723958207563842249L;
 	private static final int WITDH = 200;
 	private static final int HEIGHT = 200;
 	
 	private final DataController controller;
 	 
-	public ParallelCoordinatesJPanel(DataController controller) {
+	public PCoordinatesJPanel(DataController controller) {
 		this.setPreferredSize(new Dimension(WITDH, HEIGHT));
 	   this.controller = controller;
    }
