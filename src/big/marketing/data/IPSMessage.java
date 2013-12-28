93674e08b6d1193b6f31e3f19fce1049fde3b3fb
oschwede <oschwede@mail.upb.de>
2013-12-28 23:44:38 +0100

    more parsing logic for IPSMessage, reduced amount of data stored to the DB

--- src/big/marketing/data/IPSMessage.java
+++ src/big/marketing/data/IPSMessage.java
@@ -1,67 +1,138 @@
 package big.marketing.data;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
 
 public class IPSMessage implements DBWritable {
 	
-	public static final int DIRECTION_IN=0,DIRECTION_OUT=1,DIRECTION_EMPTY=2;
+	private boolean important;
 	
-	private final String dateTime,
+	private final String 
+		destService,
+		flags;
+		
+	private final int 
+		direction,
+		protocol,
 		priority,
 		operation,
-		messageCode,
-		protocol,
-		destinationService,
-		flags;
-	
-	private final int direction,
 		sourceIP,
-		destinationIP,
+		destIP,
 		sourcePort,
-		destinationPort;
+		destPort;
 	
+	private final long unixTime;
 	
-	
+	public static final int
+		DIRECTION_IN=0,
+		DIRECTION_OUT=1,
+		DIRECTION_EMPTY=2,
+		
+		PROTOCOL_UDP=0,
+		PROTOCOL_TCP=1,
+		
+		PRIORITY_WARNING=0,
+		PRIORITY_INFO=1,
+		
+		OPERATION_DENY=0,
+		OPERATION_TEARDOWN=1,
+		OPERATION_BUILT=2;
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MMM/yyyy kk:mm:ss"); 
 	public IPSMessage(String ... args) {
 		super();
-		this.dateTime = args[0];
-		this.priority = args[1];
-		this.operation = args[2];
-		this.messageCode = args[3];
-		this.protocol = args[4];
-		this.sourceIP = encodeIP(args[5]);
-		this.destinationIP = encodeIP(args[6]);
-		this.sourcePort = Integer.parseInt(args[7]);
-		this.destinationPort = Integer.parseInt(args[8]);
-		this.destinationService = args[9];
+
+		Date d=null;
+		try {
+			d = sdf.parse(args[0]);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		unixTime = d.getTime()/1000L;
+				
+		if ("UDP".equals(args[4]))
+			this.protocol = PROTOCOL_UDP;
+		else if ("TCP".equals(args[4]))
+			this.protocol = PROTOCOL_TCP;
+		else{
+			System.out.println("Invalid Protocol: "+args[4]);
+			this.protocol = -1;
+		}
 		
-		if ("in".compareToIgnoreCase(args[10])==0){
+		if ("Warning".equals(args[1]))
+			this.priority = PRIORITY_WARNING;
+		else if ("Info".equals(args[1]))
+			this.priority = PRIORITY_INFO;
+		else{
+			System.out.println("Invalid Priority: "+args[1]);
+			this.priority = -1;
+		}
+		important = false;
+		
+		// keep only Deny messages, the others are normal network traffic,
+		// according to the manual
+		if ("Deny".equals(args[2])){
+			this.operation = OPERATION_DENY;
+			important = true;
+		}
+		else if ("Teardown".equals(args[2]))
+			this.operation = OPERATION_TEARDOWN;
+		else if ("Built".equals(args[2])){
+			this.operation = OPERATION_BUILT;		
+		}else{
+			System.out.println("Invalid Operation: "+args[2]);
+			this.operation = -1;
+		}
+		
+		// args[3] contains the message code, which contains only redundant information
+		// so dont store the message code
+		
+		// important message codes:
+		// "ASA-6-106015" -> deny tcp packet without connection
+		// "ASA-4-106023" -> deny footprinting or port scanning
+		
+		this.sourceIP = encodeIP(args[5]);					
+		this.destIP = encodeIP(args[6]);				
+		this.sourcePort = Integer.parseInt(args[7]);		
+		this.destPort = Integer.parseInt(args[8]);	
+		
+		this.destService = args[9];					
+		
+		if (args[10].startsWith("in")){			
 			this.direction = DIRECTION_IN;
-		}else if ("out".compareToIgnoreCase(args[10])==0)
+		}else if (args[10].startsWith("out"))
 			this.direction = DIRECTION_OUT;
-		else
+		else if (args[10].contains("empty"))
 			this.direction = DIRECTION_EMPTY;
+		else{
+			System.out.println("Invalid Direction: "+args[10]);
+			this.direction = -1;
+		}
 		
-		this.flags = args[11];
+		this.flags = args[11];								
 	}
 
 	@Override
 	public DBObject asDBObject() {
+		if (!important)
+			return null;
 		BasicDBObject dbo = new BasicDBObject();
-		// TODO: remove unused features
-		dbo.append("Time", dateTime);
+		// maybe remove some more features here?
+		dbo.append("Time", unixTime);
 		dbo.append("SourceIP", sourceIP);
-		dbo.append("DestIP", destinationIP);
-		dbo.append("Protocol", protocol);
+		dbo.append("DestIP", destIP);
+//		dbo.append("MessageCode", messageCode);
+//		dbo.append("Protocol", protocol);
 		dbo.append("sourcePort", sourcePort);
-		dbo.append("destinationPort", destinationPort);
-		dbo.append("Priority", priority);
-		dbo.append("Operation", operation);
-		dbo.append("MessageCode", messageCode);
-		dbo.append("DestinationService", destinationService);
-		dbo.append("Direction", direction);
-		dbo.append("Flags", flags);
+		dbo.append("destinationPort", destPort);
+//		dbo.append("Priority", priority); // 9000000 vs 7500000
+//		dbo.append("Operation", operation);  // this is always 2 , operation deny
+//		dbo.append("DestinationService", destService);
+//		dbo.append("Direction", direction); // this is always (empty)...
+//		dbo.append("Flags", flags);
 		return dbo;
 	}
 
