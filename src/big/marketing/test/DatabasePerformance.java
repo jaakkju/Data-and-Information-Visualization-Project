90fdc4dfd740f04a562103de8f73c51371b06da2
oschwede <oschwede@mail.upb.de>
2014-01-03 00:20:04 +0100

    more performance stats

--- src/big/marketing/test/DatabasePerformance.java
+++ src/big/marketing/test/DatabasePerformance.java
@@ -1,5 +1,6 @@
 package big.marketing.test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
@@ -17,19 +18,21 @@ public class DatabasePerformance {
 		// Testing performance of Queries
 		Settings.loadConfig();
 		MongoController mc = new MongoController();
-		for (DataType t : DataType.values()){
-			testPerformance(t, mc,10,10,1000);
-		}
+//		for (DataType t : DataType.values()){
+//			if (t != DataType.FLOW)
+//				testPerformance(t, mc,	10,10,1000);
+//		}
+		testPerformance(DataType.FLOW, mc, 10, 3600*12, 3600*12);
 		
 	}
 	
 	public static void testPerformance(DataType t, MongoController mc, int times, int intervalMin, int intervalMax){
 		
 		int minTime = 1364830798, maxTime = 1366012800;
-		long totalTime=0;
 		long turnTime=0;
+		int objects=0;
+		List<Long> vals = new ArrayList<Long>(times);
 		for (int i = 0; i < times; i++) {
-			
 			// How many time units should be queried
 			int intervalLength = (int) Math.ceil(Math.random() * (intervalMax-intervalMin) + intervalMin);
 			
@@ -38,17 +41,29 @@ public class DatabasePerformance {
 			int end = start + intervalLength;
 			
 			long s = System.currentTimeMillis();
-			logger.info("Starting Time-Query: " + start + " to " + end);
+//			logger.info("Starting Time-Query: " + start + " to " + end);
 			List<DBObject> test;
 			test = mc.getConstrainedEntries(t, "Time", start, end);
 
 			turnTime = System.currentTimeMillis() - s;
-			totalTime += turnTime;
-//			logger.info("Finish querying " + intervalLength + " time units in "
-//					+ t.name() + ". Objects: " + test.size() + " Duration: "
-//					+ turnTime + " ms");
+			vals.add(turnTime);
+			objects += test.size();
+			logger.info("Finish querying " + intervalLength + " time units in "
+					+ t.name() + ". Objects: " + test.size() + " Duration: "
+					+ turnTime + " ms");
 
 		}
-		logger.info(t.name()+" total time: "+totalTime+"ms  Avg Time: "+(totalTime/times)+" ms");
+		long totalTime=0;
+		for (Long val : vals){
+			totalTime+=val;
+		}
+		long avgTime = totalTime / times;
+		long stddev = 0;
+		for (Long val : vals){
+			stddev += (val-avgTime)*(val-avgTime);
+		}
+		stddev = (int) Math.sqrt(stddev);
+		logger.info(t.name()+" total time: "+totalTime+"ms Objects:"+objects + " obj/ms: "+objects/totalTime);
+		logger.info("Avg Time: "+(totalTime/times)+" ms Stddev: "+stddev+"ms");
 	}
 }
