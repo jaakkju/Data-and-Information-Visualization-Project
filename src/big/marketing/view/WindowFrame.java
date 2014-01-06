055a9af53e5c6b5083ab19459474ad89c20ece6f
oschwede <oschwede@mail.upb.de>
2014-01-06 22:03:05 +0100

    reorganized gui

--- src/big/marketing/view/WindowFrame.java
+++ src/big/marketing/view/WindowFrame.java
@@ -1,8 +1,13 @@
 package big.marketing.view;
 
+import java.awt.Component;
+import java.awt.Container;
 import java.awt.Dimension;
-import java.awt.GridLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 
 import big.marketing.Settings;
@@ -12,16 +17,19 @@ public class WindowFrame extends JFrame {
 	private static int FRAME_WIDTH = 1200;
 	private static int FRAME_HEIGHT = 600;
 	private static final String FRAME_TITLE = "eyeNet - Network Monitor";
-	private static final int rows = 2;
-	private static final int cols = 2;
 	
 	
-	public WindowFrame() {
+	public WindowFrame(JComponent graphPanel, JComponent pCoordinatesPanel, JComponent controlsPanel) {
 		loadSettings();
 		this.setTitle(FRAME_TITLE);
 		this.setPreferredSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setLayout(new GridLayout(rows, cols));
+		GridBagLayout gbl = new GridBagLayout();
+		this.setLayout(gbl);
+		addComponent(getContentPane(), gbl, graphPanel, 0, 0, 1, 1, 0.5, 0.5);
+		addComponent(getContentPane(), gbl, pCoordinatesPanel, 0, 1, 1, 1, 0.5, 0.5);
+		addComponent(getContentPane(), gbl, controlsPanel, 0, 2, 1, 1, 0, 0);
+		
 		this.pack();
 		this.setVisible(true);
    }
@@ -31,4 +39,19 @@ public class WindowFrame extends JFrame {
 		FRAME_WIDTH= Settings.getInt("view.frame.width");
 	}
 	
+	static void addComponent(Container cont, GridBagLayout gbl, Component c,
+			int x, int y, int width, int height, double weightx, double weighty) {
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.gridx = x;
+		gbc.gridy = y;
+		gbc.gridwidth = width;
+		gbc.gridheight = height;
+		gbc.weightx = weightx;
+		gbc.weighty = weighty;
+		int border = 2;
+		gbc.insets = new Insets(border / 2, border, border / 2, border);
+		gbl.setConstraints(c, gbc);
+		cont.add(c);
+	}
 }
