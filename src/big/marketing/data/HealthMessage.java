f3a6921bc7600f3beed5c414c5b3505683ffd34e
Juhani Jaakkola <juhani@jaakkola.net>
2014-01-09 12:02:37 +0100

    added some more fields to health message

--- src/big/marketing/data/HealthMessage.java
+++ src/big/marketing/data/HealthMessage.java
@@ -17,14 +17,18 @@ public class HealthMessage implements DBWritable {
 	public static final int CONN_OK = 1;
 	public static final int CONN_NOK = 0;
 
+	//	private final int id;
 	private final String hostname;
-	private final int statusVal;
+	private final String serviceName;
 	private final int time;
+	private final int statusVal;
+	private final String receivedFrom;
 	private final int diskUsage;
 	private final int pageFileUsage;
 	private final int numProcs;
 	private final int loadAverage;
 	private final int physicalMemoryUsage;
+	private final int connMade;
 
 	@Override
 	public String toString() {
@@ -35,13 +39,16 @@ public class HealthMessage implements DBWritable {
 
 	public HealthMessage(DBObject dbo) {
 		this.hostname = (String) dbo.get("hostname");
+		this.serviceName = (String) dbo.get("serviceName");
 		this.statusVal = (Integer) dbo.get("statusVal");
+		this.receivedFrom = (String) dbo.get("receivedFrom");
 		this.time = (Integer) dbo.get("time");
 		this.diskUsage = (Integer) dbo.get("diskUsage");
 		this.pageFileUsage = (Integer) dbo.get("pageFileUsage");
 		this.numProcs = (Integer) dbo.get("numProcs");
 		this.loadAverage = (Integer) dbo.get("loadAverage");
 		this.physicalMemoryUsage = (Integer) dbo.get("physicalMemoryUsage");
+		this.connMade = (Integer) dbo.get("connMade");
 	}
 
 	/**
@@ -53,19 +60,19 @@ public class HealthMessage implements DBWritable {
 
 		//		this.id = argsToInt(args, 0);
 		this.hostname = argsToString(args, 1);
-		//		this.serviceName = argsToString(args, 2);
+		this.serviceName = argsToString(args, 2);
 		int timeSeconds = (int) Double.parseDouble(args[3]);
 		this.time = (timeSeconds / 60) * 60;
 
 		this.statusVal = argsToInt(args, 4);
 		//		args[5] is the unparsed message content
-		//		this.receivedFrom = argsToString(args, 6);
+		this.receivedFrom = argsToString(args, 6);
 		this.diskUsage = argsToInt(args, 7);
 		this.pageFileUsage = argsToInt(args, 8);
 		this.numProcs = argsToInt(args, 9);
 		this.loadAverage = argsToInt(args, 10);
 		this.physicalMemoryUsage = argsToInt(args, 11);
-		//		this.connMade = argsToInt(args, 12);
+		this.connMade = argsToInt(args, 12);
 		// args[13] is parsedDate from message content
 	}
 
@@ -87,8 +94,16 @@ public class HealthMessage implements DBWritable {
 		return hostname;
 	}
 
+	public String getServiceName() {
+		return serviceName;
+	}
+
 	public int getStatusVal() {
 		return statusVal;
+	}
+
+	public String getReceivedFrom() {
+		return receivedFrom;
 	}
 
 	public int getTime() {
@@ -115,16 +130,23 @@ public class HealthMessage implements DBWritable {
 		return physicalMemoryUsage;
 	}
 
+	public int getConnMade() {
+		return connMade;
+	}
+
 	public DBObject asDBObject() {
 		BasicDBObject bdbo = new BasicDBObject();
 		bdbo.append("hostname", hostname);
+		bdbo.append("serviceName", serviceName);
 		bdbo.append("statusVal", statusVal);
+		bdbo.append("receivedFrom", receivedFrom);
 		bdbo.append("time", time);
 		bdbo.append("diskUsage", diskUsage);
 		bdbo.append("pageFileUsage", pageFileUsage);
 		bdbo.append("numProcs", numProcs);
 		bdbo.append("loadAverage", loadAverage);
 		bdbo.append("physicalMemoryUsage", physicalMemoryUsage);
+		bdbo.append("connMade", connMade);
 		return bdbo;
 	}
 }
