e50d522416ee4c442223cfcae6465f92442ac815
Juhani Jaakkola <juhani@jaakkola.net>
2014-01-09 18:49:54 +0100

    update to slider and added play and stop buttons

--- src/big/marketing/view/ControlsJPanel.java
+++ src/big/marketing/view/ControlsJPanel.java
@@ -1,10 +1,13 @@
 package big.marketing.view;
 
 import java.awt.BorderLayout;
-import java.awt.Font;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.event.ChangeEvent;
@@ -19,6 +22,8 @@ public class ControlsJPanel extends JPanel implements Observer {
 	private static final long serialVersionUID = 7478563340170330453L;
 	private final DataController controller;
 	private JSlider qWindowSlider;
+	private JPanel buttonPanel;
+	private JButton playButton, stopButton;
 
 	static Logger logger = Logger.getLogger(ControlsJPanel.class);
 	private static int QW_MIN = 0, QW_MAX = 1217384;
@@ -28,7 +33,36 @@ public class ControlsJPanel extends JPanel implements Observer {
 		this.controller = controller;
 		this.setLayout(new BorderLayout());
 
+		playButton = new JButton("Play");
+		playButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				logger.info("Play button press");
+			}
+		});
+		add(playButton, BorderLayout.LINE_START);
+
+		stopButton = new JButton("Stop");
+		stopButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				logger.info("Stop button press");
+			}
+		});
+		add(stopButton, BorderLayout.LINE_START);
+
+		buttonPanel = new JPanel(new FlowLayout());
+		buttonPanel.add(playButton);
+		buttonPanel.add(stopButton);
+		add(buttonPanel, BorderLayout.LINE_START);
+
 		qWindowSlider = new JSlider(JSlider.HORIZONTAL, QW_MIN, QW_MAX, QW_MAX / 2);
+		qWindowSlider.setMajorTickSpacing(100000);
+		qWindowSlider.setMinorTickSpacing(10000);
+		qWindowSlider.setPaintTicks(true);
+		qWindowSlider.setPaintLabels(true);
 		qWindowSlider.addChangeListener(new ChangeListener() {
 
 			@Override
@@ -39,10 +73,6 @@ public class ControlsJPanel extends JPanel implements Observer {
 				}
 			}
 		});
-
-		Font font = new Font("Serif", Font.ITALIC, 15);
-		qWindowSlider.setFont(font);
-
 		add(qWindowSlider);
 	}
 
