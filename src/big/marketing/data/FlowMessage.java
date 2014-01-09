210832b42e2c73503327cd214334d00be9f229d5
oschwede <oschwede@yskalnari.cs.uni-paderborn.de>
2014-01-09 18:26:51 +0100

    code cleanup

--- src/big/marketing/data/FlowMessage.java
+++ src/big/marketing/data/FlowMessage.java
@@ -6,17 +6,11 @@ import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
 
-public class FlowMessage implements DBWritable{
+public class FlowMessage implements DBWritable {
 	static Logger logger = Logger.getLogger(FlowMessage.class);
-	
-	/**
-	 * time in Unixtime
-	 * range: [1364802616,1366020000]
-	 */
+
 	private final int time;
-	
-	
-//	private final int protocol;
+	// private final int protocol;
 	private final int srcIP;
 	private final int dstIP;
 	private final int srcPort;
@@ -26,46 +20,25 @@ public class FlowMessage implements DBWritable{
 	private final int packetCount;
 	private final int srcPortCount;
 	private final int dstPortCount;
-//	private final boolean hasMoreFragments;
-//	private final boolean hasSubsequentFragments;
 	private final int duration;
-//	private final int sourcePayloadBytes;
-//	private final int destinationPayloadBytes;
-//	private final int sourceTotalBytes;
-//	private final int destinationTotalBytes;
-//	private final int sourcePacketCount;
-//	private final int destinationPacketCount;
 
 	public FlowMessage(String[] args) {
 		super();
-		// TODO: remove unused features
 		int timeSeconds = (int) Double.parseDouble(args[0]);
 		this.time = (timeSeconds / 60) * 60;
 		// this.protocol = Integer.parseInt(args[3]);
-		this.srcIP = encodeIP(args[5]); 
-		this.dstIP = encodeIP(args[6]); 
+		this.srcIP = encodeIP(args[5]);
+		this.dstIP = encodeIP(args[6]);
 		this.srcPort = Integer.parseInt(args[7]);
 		this.dstPort = Integer.parseInt(args[8]);
-		this.srcPortCount=1;
-		this.dstPortCount=1;
-		
-		// 0 0 -> flow with just one fragment
-		// 0 1 -> last fragment
-		// 1 0 -> first fragment
-		// 1 1 -> intermediate fragment (not first nor last fragment)
-//		this.hasMoreFragments = "1".equals(args[9]); // 0 or 1
-//		this.hasSubsequentFragments = "1".equals(args[10]); // 0 or 1
+		this.srcPortCount = 1;
+		this.dstPortCount = 1;
+
 		this.duration = Integer.parseInt(args[11]);
-//		this.sourcePayloadBytes = Integer.parseInt(args[12]);
-//		this.destinationPayloadBytes = Integer.parseInt(args[13]);
-//		this.sourceTotalBytes = Integer.parseInt(args[14]);
-//		this.destinationTotalBytes = Integer.parseInt(args[15]);
-//		this.sourcePacketCount = Integer.parseInt(args[16]);
-//		this.destinationPacketCount = Integer.parseInt(args[17]);
-		this.payloadBytes = Integer.parseInt(args[12]) +Integer.parseInt(args[13]);
-		this.totalBytes = Integer.parseInt(args[14]) +Integer.parseInt(args[15]);
-		this.packetCount = Integer.parseInt(args[16]) +Integer.parseInt(args[17]);
-		
+		this.payloadBytes = Integer.parseInt(args[12]) + Integer.parseInt(args[13]);
+		this.totalBytes = Integer.parseInt(args[14]) + Integer.parseInt(args[15]);
+		this.packetCount = Integer.parseInt(args[16]) + Integer.parseInt(args[17]);
+
 	}
 
 	public FlowMessage(DBObject dbo) {
@@ -81,41 +54,59 @@ public class FlowMessage implements DBWritable{
 		this.totalBytes = convertToInt(dbo.get("totalBytes"));
 		this.packetCount = convertToInt(dbo.get("packetCount"));
 	}
-	
-	private int arrayToInt(Object o){
-		if (o instanceof BasicDBList){
+
+	private int arrayToInt(Object o) {
+		if (o instanceof BasicDBList) {
 			BasicDBList list = (BasicDBList) o;
 			return list.size();
-		}else{
+		} else {
 			throw new IllegalArgumentException();
 		}
 	}
-	
+
 	private int convertToInt(Object o) {
 		if (o instanceof Double) {
 			return ((Double) o).intValue();
 		} else if (o instanceof Integer) {
 			return ((Integer) o).intValue();
-		} else if (o instanceof BasicDBList){
+		} else if (o instanceof BasicDBList) {
 			BasicDBList list = (BasicDBList) o;
-			if (list.size() > 0){
+			if (list.size() > 0) {
 				return ((Integer) list.get(0)).intValue();
-			}else
+			} else
 				return 0;
-		}else
+		} else
 			try {
 				return Integer.parseInt((String) o);
 			} catch (Exception e) {
 				return 0;
 			}
 	}
+
+	public DBObject asDBObject() {
+		BasicDBObject dbo = new BasicDBObject();
+		dbo.append("time", time); // reduce time resolution to minute
+		dbo.append("srcIP", srcIP);
+		dbo.append("dstIP", dstIP);
+		// dbo.append("Protocol", protocol);
+		dbo.append("srcPort", srcPort);
+		dbo.append("dstPort", dstPort);
+		dbo.append("duration", duration);
+
+		dbo.append("payloadBytes", payloadBytes);
+		dbo.append("totalBytes", totalBytes);
+		dbo.append("packetCount", packetCount);
+
+		return dbo;
+	}
+
 	public int getTime() {
 		return time;
 	}
 
-//	public int getProtocol() {
-//		return protocol;
-//	}
+	// public int getProtocol() {
+	// return protocol;
+	// }
 
 	public String getSourceIP() {
 		return decodeIP(srcIP);
@@ -141,84 +132,36 @@ public class FlowMessage implements DBWritable{
 		return dstPortCount;
 	}
 
-	// public boolean hasMoreFragments() {
-	// return hasMoreFragments;
-//	}
-//
-//	public boolean hasSubsequentFragments() {
-//		return hasSubsequentFragments;
-//	}
-
 	public int getDuration() {
 		return duration;
 	}
 
-//	public int getSourcePayloadBytes() {
-//		return sourcePayloadBytes;
-//	}
-//
-//	public int getDestinationPayloadBytes() {
-//		return destinationPayloadBytes;
-//	}
-//
-//	public int getSourceTotalBytes() {
-//		return sourceTotalBytes;
-//	}
-//
-//	public int getDestinationTotalBytes() {
-//		return destinationTotalBytes;
-//	}
-//
-//	public int getSourcePacketCount() {
-//		return sourcePacketCount;
-//	}
-//
-//	public int getDestinationPacketCount() {
-//		return destinationPacketCount;
-//	}
-
-	private static String decodeIP(int ip){
-		String out ="";
-		for (int i=3;i>=0;i--){
-			 out += ((ip >> (i*8)) & 255) + ".";
-		}
-		return out.substring(0, out.length()-1);
+	public int getPayloadBytes() {
+		return payloadBytes;
 	}
+
+	public int getTotalBytes() {
+		return totalBytes;
+	}
+
+	public int getPacketCount() {
+		return packetCount;
+	}
+
+	private static String decodeIP(int ip) {
+		String out = "";
+		for (int i = 3; i >= 0; i--) {
+			out += ((ip >> (i * 8)) & 255) + ".";
+		}
+		return out.substring(0, out.length() - 1);
+	}
+
 	private static int encodeIP(String strIP) {
 		int ipNum = 0;
 		String[] split = strIP.split("\\.");
-		for (int i = 0; i < split.length; i++){
-			ipNum += Integer.parseInt(split[split.length-1-i]) << (i*8);
+		for (int i = 0; i < split.length; i++) {
+			ipNum += Integer.parseInt(split[split.length - 1 - i]) << (i * 8);
 		}
 		return ipNum;
-	}
-	public DBObject asDBObject(){
-		BasicDBObject dbo = new BasicDBObject();
-		// TODO: remove unused features
-		dbo.append("time", time); // reduce time resolution to minute
-		dbo.append("srcIP", srcIP);
-		dbo.append("dstIP", dstIP);
-//		dbo.append("Protocol", protocol);
-		dbo.append("srcPort", srcPort);
-		dbo.append("dstPort", dstPort);
-		dbo.append("duration", duration);
-//		dbo.append("hasSubsequentFragments", hasSubsequentFragments);
-//		dbo.append("hasMoreFragments", hasMoreFragments);
-		
-		dbo.append("payloadBytes", payloadBytes);
-		dbo.append("totalBytes", totalBytes);
-		dbo.append("packetCount", packetCount);
-		
-//		dbo.append("payloadBytes", sourcePayloadBytes+destinationPayloadBytes);
-//		dbo.append("totalBytes", sourceTotalBytes+destinationTotalBytes);
-//		dbo.append("packetCount", sourcePacketCount+destinationPacketCount);
-		
-//		dbo.append("srcPayload", sourcePayloadBytes);
-//		dbo.append("destPayload", destinationPayloadBytes);
-//		dbo.append("srcTotal", sourceTotalBytes);
-//		dbo.append("destTotal", destinationTotalBytes);
-//		dbo.append("sourcePackets", sourcePacketCount);
-//		dbo.append("destinationPackets", destinationPacketCount);
-		return dbo;
 	}
 }
--- src/big/marketing/data/IPSMessage.java
+++ src/big/marketing/data/IPSMessage.java
@@ -13,64 +13,47 @@ import com.mongodb.DBObject;
 public class IPSMessage implements DBWritable {
 	static Logger logger = Logger.getLogger(IPSMessage.class);
 
-	//	private final String 
-	//		destService,
-	//		flags;
+	private final int time;
+	private final int operation;
+	private final int srcIP;
+	private final int dstIP;
+	private final int srcPort;
+	private final int dstPort;
 
-	private final int time,
-	//		direction,
-	//		protocol,
-	//		priority,
-	      operation, srcIP, dstIP, srcPort, dstPort;
+	public static final int OPERATION_DENY = 0;
+	public static final int OPERATION_TEARDOWN = 1;
+	public static final int OPERATION_BUILT = 2;
 
-	//	public String getDestService() {
-	//		return destService;
-	//	}
-	//
-	//	public String getFlags() {
-	//		return flags;
-	//	}
+	private static SimpleDateFormat dateParser = new SimpleDateFormat("dd/MMM/yyyy HH:mm:ss", Locale.ENGLISH);
 
-	public int getTime() {
-		return time;
+	public IPSMessage(String... args) throws ParseException {
+		super();
+
+		Date d = null;
+		d = dateParser.parse(args[0]);
+
+		int timeSeconds = (int) (d.getTime() / 1000L);
+		this.time = (timeSeconds / 60) * 60;
+
+		// keep only Deny messages, the others are normal network traffic,
+		// according to the manual
+		if ("Deny".equals(args[2]))
+			this.operation = OPERATION_DENY;
+		else if ("Teardown".equals(args[2]))
+			this.operation = OPERATION_TEARDOWN;
+		else if ("Built".equals(args[2])) {
+			this.operation = OPERATION_BUILT;
+		} else {
+			System.out.println("Invalid Operation: " + args[2]);
+			this.operation = -1;
+		}
+
+		this.srcIP = encodeIP(args[5]);
+		this.dstIP = encodeIP(args[6]);
+		this.srcPort = Integer.parseInt(args[7]);
+		this.dstPort = Integer.parseInt(args[8]);
+
 	}
-
-	//	public int getDirection() {
-	//		return direction;
-	//	}
-	//
-	//	public int getProtocol() {
-	//		return protocol;
-	//	}
-	//
-	//	public int getPriority() {
-	//		return priority;
-	//	}
-	//
-	//	public int getOperation() {
-	//		return operation;
-	//	}
-
-	public String getSourceIP() {
-		return decodeIP(srcIP);
-	}
-
-	public String getDestinationIP() {
-		return decodeIP(dstIP);
-	}
-
-	public int getSourcePort() {
-		return srcPort;
-	}
-
-	public int getDestinationPort() {
-		return dstPort;
-	}
-
-	public static final int DIRECTION_IN = 0, DIRECTION_OUT = 1, DIRECTION_EMPTY = 2, PROTOCOL_UDP = 0, PROTOCOL_TCP = 1,
-	      PRIORITY_WARNING = 0, PRIORITY_INFO = 1, OPERATION_DENY = 0, OPERATION_TEARDOWN = 1, OPERATION_BUILT = 2;
-
-	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MMM/yyyy HH:mm:ss", Locale.ENGLISH);
 
 	public IPSMessage(DBObject dbo) {
 		operation = OPERATION_DENY;
@@ -94,95 +77,42 @@ public class IPSMessage implements DBWritable {
 			}
 	}
 
-	public IPSMessage(String... args) throws ParseException {
-		super();
-
-		Date d = null;
-		d = sdf.parse(args[0]);
-
-		int timeSeconds = (int) (d.getTime() / 1000L);
-		this.time = (timeSeconds / 60) * 60;
-
-		//		if ("UDP".equals(args[4]))
-		//			this.protocol = PROTOCOL_UDP;
-		//		else if ("TCP".equals(args[4]))
-		//			this.protocol = PROTOCOL_TCP;
-		//		else{
-		//			System.out.println("Invalid Protocol: "+args[4]);
-		//			this.protocol = -1;
-		//		}
-		//		
-		//		if ("Warning".equals(args[1]))
-		//			this.priority = PRIORITY_WARNING;
-		//		else if ("Info".equals(args[1]))
-		//			this.priority = PRIORITY_INFO;
-		//		else{
-		//			System.out.println("Invalid Priority: "+args[1]);
-		//			this.priority = -1;
-		//		}
-
-		// keep only Deny messages, the others are normal network traffic,
-		// according to the manual
-		if ("Deny".equals(args[2]))
-			this.operation = OPERATION_DENY;
-		else if ("Teardown".equals(args[2]))
-			this.operation = OPERATION_TEARDOWN;
-		else if ("Built".equals(args[2])) {
-			this.operation = OPERATION_BUILT;
-		} else {
-			System.out.println("Invalid Operation: " + args[2]);
-			this.operation = -1;
-		}
-
-		// args[3] contains the message code, which contains only redundant information
-		// so dont store the message code
-
-		// important message codes:
-		// "ASA-6-106015" -> deny tcp packet without connection
-		// "ASA-4-106023" -> deny footprinting or port scanning
-
-		this.srcIP = encodeIP(args[5]);
-		this.dstIP = encodeIP(args[6]);
-		this.srcPort = Integer.parseInt(args[7]);
-		this.dstPort = Integer.parseInt(args[8]);
-
-		//		this.destService = args[9];					
-		//		
-		//		if (args[10].startsWith("in")){			
-		//			this.direction = DIRECTION_IN;
-		//		}else if (args[10].startsWith("out"))
-		//			this.direction = DIRECTION_OUT;
-		//		else if (args[10].contains("empty"))
-		//			this.direction = DIRECTION_EMPTY;
-		//		else{
-		//			System.out.println("Invalid Direction: "+args[10]);
-		//			this.direction = -1;
-		//		}
-		//		
-		//		this.flags = args[11];								
-	}
-
 	@Override
 	public DBObject asDBObject() {
 
 		if (operation != OPERATION_DENY)
 			return null;
-
 		BasicDBObject dbo = new BasicDBObject();
-		// maybe remove some more features here?
 		dbo.append("time", (time / 60) * 60);
 		dbo.append("srcIP", srcIP);
 		dbo.append("dstIP", dstIP);
-		//		dbo.append("MessageCode", messageCode);
-		//		dbo.append("Protocol", protocol);
 		dbo.append("srcPort", srcPort);
 		dbo.append("dstPort", dstPort);
-		//		dbo.append("Priority", priority); // 9000000 vs 7500000
-		//		dbo.append("Operation", operation);  // this is always 2 , operation deny
-		//		dbo.append("DestinationService", destService);
-		//		dbo.append("Direction", direction); // this is always (empty)...
-		//		dbo.append("Flags", flags);
 		return dbo;
+	}
+
+	public int getTime() {
+		return time;
+	}
+
+	public int getOperation() {
+		return operation;
+	}
+
+	public String getSourceIP() {
+		return decodeIP(srcIP);
+	}
+
+	public String getDestinationIP() {
+		return decodeIP(dstIP);
+	}
+
+	public int getSourcePort() {
+		return srcPort;
+	}
+
+	public int getDestinationPort() {
+		return dstPort;
 	}
 
 	private static String decodeIP(int ip) {
